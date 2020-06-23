c5b18c0061ac9a0003b572409724ac45e28be5d3
grtlinux <grtlinux@gmail.com>
2020-06-24 08:56:44 +0900

    create 1st

---
+++ ver-05.01/KieaLightnet-05-batch-01/src/main/java/org/tain/KieaLightnet05Batch01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnet05Batch01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnet05Batch01Application.class, args);
+	}
+
+}
