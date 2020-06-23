676040798d60847bd045726e72dc32fb6c05d2d5
grtlinux <grtlinux@gmail.com>
2020-06-23 22:54:01 +0900

    create 1st

---
+++ ver-02.01/KieaLightnet-02-link-01/src/main/java/org/tain/KieaLightnet02Link01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnet02Link01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnet02Link01Application.class, args);
+	}
+
+}
---
+++ ver-02.01/KieaLightnet-02-link-01/src/test/java/org/tain/KieaLightnet02Link01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaLightnet02Link01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
