45f2ec5615125122e39f3018e760c014f2360c99
grtlinux <grtlinux@gmail.com>
2020-06-23 21:17:34 +0900

    create repository

---
+++ ver-01.01/KieaLightnet-01-auth-01/src/test/java/org/tain/KieaLightnet01Auth01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaLightnet01Auth01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
