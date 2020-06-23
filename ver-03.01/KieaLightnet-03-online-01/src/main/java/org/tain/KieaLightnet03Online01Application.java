55ad6d117bc95c36112e3fb240c7a7f9f17a9cfc
grtlinux <grtlinux@gmail.com>
2020-06-23 23:45:59 +0900

    create 1st

---
+++ ver-03.01/KieaLightnet-03-online-01/src/main/java/org/tain/KieaLightnet03Online01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnet03Online01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnet03Online01Application.class, args);
+	}
+
+}
