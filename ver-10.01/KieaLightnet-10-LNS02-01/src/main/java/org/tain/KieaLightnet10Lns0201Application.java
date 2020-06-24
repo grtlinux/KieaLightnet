4230577b8291524c202a0c922690e657a2cddbcc
grtlinux <grtlinux@gmail.com>
2020-06-24 09:04:28 +0900

    create 1st

---
+++ ver-10.01/KieaLightnet-10-LNS02-01/src/main/java/org/tain/KieaLightnet10Lns0201Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnet10Lns0201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnet10Lns0201Application.class, args);
+	}
+
+}
