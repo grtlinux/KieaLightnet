b19872cb4ed2732e69210d705d656919d05c5ecf
grtlinux <grtlinux@gmail.com>
2020-06-27 20:47:16 +0900

    create 1st

---
+++ ver-12.01/KieaLightnet-bootstrap-with-springboot/src/main/java/org/tain/KieaLightnetBootstrapWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnetBootstrapWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnetBootstrapWithSpringbootApplication.class, args);
+	}
+
+}
