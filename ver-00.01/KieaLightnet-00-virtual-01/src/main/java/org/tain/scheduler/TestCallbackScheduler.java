64c1715c1fa564bd4bdd59811631b08fe719f4b3
grtlinux <grtlinux@gmail.com>
2020-07-14 18:51:54 +0900

    ...

--- ver-00.01/KieaLightnet-00-virtual-01/src/main/java/org/tain/scheduler/TestCallbackScheduler.java
+++ ver-00.01/KieaLightnet-00-virtual-01/src/main/java/org/tain/scheduler/TestCallbackScheduler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.tain.config.SkipSSLConfig;
