71f9097854f5230f518d40f4b0ca1a9de8b0ded3
grtlinux <grtlinux@gmail.com>
2020-07-17 10:15:50 +0900

    add security

--- ver-06.01/KieaLightnet-06-info-01/src/main/java/org/tain/KieaLightnet06Info01Application.java
+++ ver-06.01/KieaLightnet-06-info-01/src/main/java/org/tain/KieaLightnet06Info01Application.java
@@ -2,6 +2,7 @@ package org.tain;
 
 import java.io.File;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.tain.domain.Lightnet;
 import org.tain.repository.LightnetRepository;
 import org.tain.utils.CurrentInfo;
@@ -53,5 +65,43 @@ public class KieaLightnet06Info01Application implements CommandLineRunner {
 		lstLightnet.forEach(entry -> {
 			this.lightnetRepository.save(entry);
 		});
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+
+	@Configuration
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	@EnableWebSecurity
+	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Bean
+		InMemoryUserDetailsManager userDetailsManager() {
+			User.UserBuilder commonUser = User.withUsername("commonUser").password("{noop}common").roles("USER");
+			User.UserBuilder havi = User.withUsername("havi").password("{noop}test").roles("USER", "ADMIN");
+
+			List<UserDetails> userDetailsList = new ArrayList<>();
+			userDetailsList.add(commonUser.build());
+			userDetailsList.add(havi.build());
+
+			return new InMemoryUserDetailsManager(userDetailsList);
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			CorsConfiguration configuration = new CorsConfiguration();
+			configuration.addAllowedOrigin(CorsConfiguration.ALL);
+			configuration.addAllowedMethod(CorsConfiguration.ALL);
+			configuration.addAllowedHeader(CorsConfiguration.ALL);
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+
+			http.httpBasic()
+					.and().authorizeRequests()
+					//.antMatchers(HttpMethod.POST, "/Boards/**").hasRole("ADMIN")
+					.anyRequest().permitAll()
+					.and().cors().configurationSource(source)
+					.and().csrf().disable();
+		}
 	}
 }
