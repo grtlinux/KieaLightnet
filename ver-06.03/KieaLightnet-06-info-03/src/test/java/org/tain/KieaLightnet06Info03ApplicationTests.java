28af13271ccca38a832ec09a19f1b70c90441a67
grtlinux <grtlinux@gmail.com>
2020-07-22 14:51:40 +0900

    create 1st

---
+++ ver-06.03/KieaLightnet-06-info-03/src/test/java/org/tain/KieaLightnet06Info03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaLightnet06Info03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
