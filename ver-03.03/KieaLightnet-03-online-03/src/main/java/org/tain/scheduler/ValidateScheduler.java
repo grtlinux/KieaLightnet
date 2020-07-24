7eb4fc3a6301e13208f7a126c6007de2b904a2f0
grtlinux <grtlinux@gmail.com>
2020-07-24 10:19:57 +0900

    interrupted

--- ver-03.03/KieaLightnet-03-online-03/src/main/java/org/tain/scheduler/ValidateScheduler.java
+++ ver-03.03/KieaLightnet-03-online-03/src/main/java/org/tain/scheduler/ValidateScheduler.java
@@ -2,6 +2,7 @@ package org.tain.scheduler;
 
 import org.tain.object.LnsPacket;
 import org.tain.utils.CurrentInfo;
+import org.tain.utils.Flag;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,7 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidateScheduler {
 
 	public static LnsPacket process(LnsPacket request) throws Exception {
-		log.info("KANG-20200623 >>>>> {} {}", CurrentInfo.get());
+		log.info("KANG-20200623 >>>>> {} request.json: {}", CurrentInfo.get(), request.toPrettyJson());
+		
+		//LnsPacket response = null;
+		if (Flag.flag) {
+			
+		}
 		
 		return request;
 	}
