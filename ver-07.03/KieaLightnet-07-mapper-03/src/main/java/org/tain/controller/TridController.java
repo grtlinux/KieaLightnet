57c2ad69f46bb2bfefcc74f71e48d8de4e534900
grtlinux <grtlinux@gmail.com>
2020-07-27 23:55:02 +0900

    ...

---
+++ ver-07.03/KieaLightnet-07-mapper-03/src/main/java/org/tain/controller/TridController.java
@@ -0,0 +1,61 @@
+package org.tain.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.tain.utils.CurrentInfo;
+import org.tain.utils.Flag;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping(value = {"/mapper/trid"})
+@Slf4j
+public class TridController {
+
+	/////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	@PostMapping(value = {"/s2j"})
+	public ResponseEntity<?> streamToJson(HttpEntity<String> _httpEntity) throws Exception {
+		log.info("KANG-20200623 >>>>> {}", CurrentInfo.get());
+		
+		if (Flag.flag) {
+			System.out.println("MAPPER >>>>> Headers = " + _httpEntity.getHeaders());
+			System.out.println("MAPPER >>>>> Body = " + _httpEntity.getBody());
+		}
+		
+		Map<String,String> map = null;
+		String data = null;
+		if (Flag.flag) {
+			/*
+			// mapping process
+			ObjectMapper objectMapper = new ObjectMapper();
+			map = objectMapper.readValue(_httpEntity.getBody(), new TypeReference<Map<String,String>>(){});
+			
+			data = map.get("data");
+			System.out.println("MAPPER >>>>> data = [" + data + "]");
+			
+			String retData = requestStreamToJson(data);
+			
+			//map.put("retData", Convert.quote(retData));
+			map.put("retData", retData);
+			System.out.println("MAPPER >>>>> map: " + map);
+			*/
+		}
+		
+		MultiValueMap<String,String> headers = new LinkedMultiValueMap<>();
+		headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+		
+		return new ResponseEntity<>(map, headers, HttpStatus.OK);
+	}
+}
