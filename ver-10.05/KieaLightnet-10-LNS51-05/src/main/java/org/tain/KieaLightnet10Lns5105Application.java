93848c172f7aa28cae99e0b054ffb992355f84f0
grtlinux <grtlinux@gmail.com>
2020-08-10 09:45:23 +0900

    create 1st

---
+++ ver-10.05/KieaLightnet-10-LNS51-05/src/main/java/org/tain/KieaLightnet10Lns5105Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnet10Lns5105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnet10Lns5105Application.class, args);
+	}
+
+}
---
+++ ver-10.05/KieaLightnet-10-LNS51-05/src/test/java/org/tain/KieaLightnet10Lns5105ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KieaLightnet10Lns5105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
