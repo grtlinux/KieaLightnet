f470ac12a9b4615e1fa6b34261dd380df383c184
grtlinux <grtlinux@gmail.com>
2020-09-04 18:34:35 +0900

    ...

--- ver-03.10/KieaLightnet-03-online-10/src/main/java/org/tain/queue/LnsStreamPacketQueue.java
+++ ver-03.10/KieaLightnet-03-online-10/src/main/java/org/tain/queue/LnsStreamPacketQueue.java
@@ -3,36 +3,25 @@ package org.tain.queue;
 import java.util.LinkedList;
 
 import org.springframework.stereotype.Component;
+import org.tain.object.lns.LnsStreamPacket;
 
 @Component
-public class WakeServerTaskQueue {
+public class LnsStreamPacketQueue {
 
-	private final LinkedList<Object> queue = new LinkedList<>();
+	private final LinkedList<LnsStreamPacket> queue = new LinkedList<>();
 	
-	public synchronized void set(Object object) {
+	public synchronized void set(LnsStreamPacket object) {
 		this.queue.addLast(object);
 		this.notifyAll();
 	}
 	
-	/*
-	public synchronized Object get() {
+	public synchronized LnsStreamPacket get() {
 		while (this.queue.size() <= 0) {
 			try {
 				wait();
 			} catch (InterruptedException e) {}
 		}
 		return this.queue.removeFirst();
-	}
-	*/
-	
-	public synchronized Object get() {
-		while (this.queue.size() <= 0) {
-			try {
-				wait();
-			} catch (InterruptedException e) {}
-		}
-		this.queue.clear();
-		return null;
 	}
 	
 	public int size() {
