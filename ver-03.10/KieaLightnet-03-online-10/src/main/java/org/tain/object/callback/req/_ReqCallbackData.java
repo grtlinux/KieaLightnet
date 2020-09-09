fa9da53f5e55f69215fabfb29924d34d575907d4
grtlinux <grtlinux@gmail.com>
2020-09-09 18:31:02 +0900

    add callback

---
+++ ver-03.10/KieaLightnet-03-online-10/src/main/java/org/tain/object/callback/req/_ReqCallbackData.java
@@ -0,0 +1,22 @@
+package org.tain.object.callback.req;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+//@JsonIgnoreProperties(value = {"idLong", "idInt"})  // to the below
+@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
+public class _ReqCallbackData {
+
+	private String counterpartyTransactionId = "82622468196393";
+	
+	private String reason = "remit completed";
+	
+	private String sourceOperatorCode = "airpay";
+	
+	private String status = "RECEIVED";
+	
+	private String transactionId = "702c74a5-ecea-4545-ae23-9afa9d8b73d1";
+}
