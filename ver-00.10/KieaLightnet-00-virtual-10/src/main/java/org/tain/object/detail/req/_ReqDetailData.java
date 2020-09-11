ab90e5a7417932863ed4309d4e8e883980ce5aba
grtlinux <grtlinux@gmail.com>
2020-09-11 21:23:31 +0900

    add sample json files

--- ver-00.10/KieaLightnet-00-virtual-10/src/main/java/org/tain/object/detail/req/_ReqDetailData.java
+++ ver-00.10/KieaLightnet-00-virtual-10/src/main/java/org/tain/object/detail/req/_ReqDetailData.java
@@ -14,11 +14,26 @@ import lombok.Data;
 public class _ReqDetailData {
 
 	@StreamAnnotation(length = 20)
-	@JsonProperty(value = "name")
+	@JsonProperty(value = "sourceCountry")
 	private String sourceCountry = "KOR";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "destinationCountry")
 	private String destinationCountry = "KHM";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "destinationOperatorCode")
 	private String destinationOperatorCode = "lyhour";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "withDrawableAmount")
 	private String withDrawableAmount = "1.500";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "transactionCurrency")
 	private String transactionCurrency = "USD";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "deliveryMethod")
 	private String deliveryMethod = "cash";
 }
