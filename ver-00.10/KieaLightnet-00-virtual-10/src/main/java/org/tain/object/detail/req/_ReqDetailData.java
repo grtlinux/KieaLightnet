933c1489940e6675b1d8206879e08cc659927bee
grtlinux <grtlinux@gmail.com>
2020-09-13 13:38:32 +0900

    be fixed on detail req

--- ver-00.10/KieaLightnet-00-virtual-10/src/main/java/org/tain/object/detail/req/_ReqDetailData.java
+++ ver-00.10/KieaLightnet-00-virtual-10/src/main/java/org/tain/object/detail/req/_ReqDetailData.java
@@ -18,20 +18,34 @@ public class _ReqDetailData {
 	private String sourceCountry = "KOR";
 	
 	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "sourceSendCurrency")
+	private String sourceSendCurrency = "THB";
+	
+	
+	@StreamAnnotation(length = 20)
 	@JsonProperty(value = "destinationCountry")
-	private String destinationCountry = "KHM";
+	private String destinationCountry = "IDN";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "destinationReceiveAmount")
+	private String destinationReceiveAmount = "1000";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "destinationReceiveCurrency")
+	private String destinationReceiveCurrency = "IDR";
 	
 	@StreamAnnotation(length = 20)
 	@JsonProperty(value = "destinationOperatorCode")
-	private String destinationOperatorCode = "lyhour";
+	private String destinationOperatorCode = "mgi";
+	
 	
 	@StreamAnnotation(length = 20)
-	@JsonProperty(value = "withDrawableAmount")
-	private String withDrawableAmount = "1.500";
+	@JsonProperty(value = "withdrawalId")
+	private String withdrawalId = "82409098";
 	
 	@StreamAnnotation(length = 20)
-	@JsonProperty(value = "transactionCurrency")
-	private String transactionCurrency = "USD";
+	@JsonProperty(value = "terminalName")
+	private String terminalName = "terminalName";
 	
 	@StreamAnnotation(length = 20)
 	@JsonProperty(value = "deliveryMethod")
