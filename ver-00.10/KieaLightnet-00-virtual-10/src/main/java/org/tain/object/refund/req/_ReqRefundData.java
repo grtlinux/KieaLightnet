c8b358923200861bf5ee82ec6f830d44d5900c01
grtlinux <grtlinux@gmail.com>
2020-09-13 13:19:55 +0900

    be fixed on refund

--- ver-00.10/KieaLightnet-00-virtual-10/src/main/java/org/tain/object/refund/req/_ReqRefundData.java
+++ ver-00.10/KieaLightnet-00-virtual-10/src/main/java/org/tain/object/refund/req/_ReqRefundData.java
@@ -11,11 +11,23 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class _ReqRefundData {
 
-	@StreamAnnotation(length = 40)
-	@JsonProperty(value = "clientId")
-	private String clientId;
+	@StreamAnnotation(length = 30)
+	@JsonProperty(value = "terminalName")
+	private String terminalName = "Testterminal-01";
 
-	@StreamAnnotation(length = 100)
-	@JsonProperty(value = "secret")
-	private String secret;
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "reason")
+	private String reason = "NO_RCV_LOC";
+
+	@StreamAnnotation(length = 5)
+	@JsonProperty(value = "feeRefFund")
+	private String feeRefFund = "Y";
+	
+	@StreamAnnotation(length = 20)
+	@JsonProperty(value = "reversalType")
+	private String reversalType = "cancel";
+
+	@StreamAnnotation(length = 30)
+	@JsonProperty(value = "transactionId")
+	private String transactionId = "4324682635157306";
 }
