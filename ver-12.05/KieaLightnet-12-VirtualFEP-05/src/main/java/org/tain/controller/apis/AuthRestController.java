24ecda0899658303fb5f0df40f16def39500e648
grtlinux <grtlinux@gmail.com>
2020-09-20 12:28:13 +0900

    add LnsHttpClient and LnsLightnetClient

--- ver-12.05/KieaLightnet-12-VirtualFEP-05/src/main/java/org/tain/controller/apis/AuthRestController.java
+++ ver-12.05/KieaLightnet-12-VirtualFEP-05/src/main/java/org/tain/controller/apis/AuthRestController.java
@@ -1,31 +1,18 @@
 package org.tain.controller.apis;
 
-import java.util.Map;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.tain.data.LnsData;
 import org.tain.object.lns.LnsJson;
-import org.tain.object.lns.LnsReqWeb;
 import org.tain.utils.CurrentInfo;
 import org.tain.utils.Flag;
-import org.tain.utils.RestTemplateConfig;
-import org.tain.utils.enums.RestTemplateType;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.tain.utils.LnsHttpClient;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -34,6 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AuthRestController {
 
+	/*
+	@Deprecated
 	@RequestMapping(value = {""}, method = {RequestMethod.GET, RequestMethod.POST})
 	public ResponseEntity<?> http(HttpEntity<String> reqHttpEntity) throws Exception {
 		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
@@ -55,6 +44,7 @@ public class AuthRestController {
 		return new ResponseEntity<>(resJson, headers, HttpStatus.OK);
 	}
 	
+	@Deprecated
 	private String auth(LnsReqWeb lnsReqWeb) throws Exception {
 		log.info("KANG-20200721 >>>>> {} {}", CurrentInfo.get());
 		
@@ -116,6 +106,7 @@ public class AuthRestController {
 		
 		return resJson;
 	}
+	*/
 	
 	///////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////
@@ -130,104 +121,20 @@ public class AuthRestController {
 			log.info("SIT >>>>> Body = {}", reqHttpEntity.getBody());
 		}
 		
-		String ret = null;
+		LnsJson lnsJson = null;
 		if (Flag.flag) {
-			LnsJson lnsJson = new LnsJson();
+			lnsJson = new LnsJson();
+			lnsJson.setName("Auth mapperReqJson2Str");
+			lnsJson.setHttpUrl("http://localhost:18086/v1.0/mapper/auth/req/j2s");
+			lnsJson.setHttpMethod("POST");
 			lnsJson.setReqJsonData(reqHttpEntity.getBody());
-			lnsJson = this.mapperReqJson2Str(lnsJson);
-			ret = "{\"reqStrData\":\"" + lnsJson.getReqStrData() + "\"}";
+			
+			lnsJson = LnsHttpClient.post(lnsJson);
 		}
 		
 		MultiValueMap<String,String> headers = new LinkedMultiValueMap<>();
 		headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
 		
-		return new ResponseEntity<>(ret, headers, HttpStatus.OK);
-	}
-	
-	private LnsJson mapperReqJson2Str(LnsJson lnsJson) throws Exception {
-		log.info("KANG-20200721 >>>>> {} {}", CurrentInfo.get());
-		
-		//String resJson = null;
-		if (Flag.flag) {
-			log.info("================== START: 1. auth(POST) ===================");
-			
-			String reqJson = null;
-			if (Flag.flag) {
-				reqJson = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(lnsJson);
-				//JsonNode jsonNode = new ObjectMapper().readTree(lnsReqWeb.getReqJson());
-				//((ObjectNode) jsonNode.at("/source")).put("transactionId", TransactionId.get());
-				//reqJson = jsonNode.toPrettyString();
-				log.info(">>>>> REQ.reqJson        = {}", reqJson);
-			}
-			
-			String httpUrl = "http://localhost:18086/v1.0/mapper/auth/req/j2s";
-			HttpMethod httpMethod = HttpMethod.POST;
-			HttpEntity<String> reqHttpEntity = null;
-			ResponseEntity<String> response = null;
-			
-			if ("POST".equalsIgnoreCase(httpMethod.name())) {
-				// post method
-				//httpUrl = lnsReqWeb.getHttpUrl();
-				//httpMethod = HttpMethod.POST;
-				log.info(">>>>> REQ.httpUrl        = {} {}", httpMethod, httpUrl);
-				
-				HttpHeaders reqHeaders = new HttpHeaders();
-				reqHeaders.setContentType(MediaType.APPLICATION_JSON);
-				//reqHeaders.set("Authorization", "Bearer " + LnsData.getInstance().getAccessToken());
-				log.info(">>>>> REQ.reqHeaders     = {}", reqHeaders);
-				
-				reqHttpEntity = new HttpEntity<>(reqJson, reqHeaders);
-			} else if ("GET".equalsIgnoreCase(httpMethod.name())) {
-				// get method
-				//httpUrl = lnsReqWeb.getHttpUrl();
-				//httpMethod = HttpMethod.GET;
-				log.info(">>>>> REQ.httpUrl 1      = {} {}", httpMethod, httpUrl);
-				
-				Map<String,String> reqMap = new ObjectMapper().readValue(reqJson, new TypeReference<Map<String,String>>(){});
-				MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-				map.setAll(reqMap);
-				
-				UriComponents builder = UriComponentsBuilder.fromHttpUrl(httpUrl)
-						.queryParams(map)
-						.build(true);
-				httpUrl = builder.toString();
-				log.info(">>>>> REQ.httpUrl 2      = {} {}", httpMethod, httpUrl);
-				
-				HttpHeaders reqHeaders = new HttpHeaders();
-				reqHeaders.setContentType(MediaType.APPLICATION_JSON);
-				//reqHeaders.set("Authorization", "Bearer " + LnsData.getInstance().getAccessToken());
-				log.info(">>>>> REQ.reqHeaders     = {}", reqHeaders);
-				
-				reqHttpEntity = new HttpEntity<>(reqHeaders);
-			}
-			
-			if (Flag.flag) {
-				try {
-					response = RestTemplateConfig.get(RestTemplateType.SETENV).exchange(
-							httpUrl
-							, httpMethod
-							, reqHttpEntity
-							, String.class);
-					
-					log.info(">>>>> RES.getStatusCodeValue() = {}", response.getStatusCodeValue());
-					log.info(">>>>> RES.getStatusCode()      = {}", response.getStatusCode());
-					log.info(">>>>> RES.getBody()            = {}", response.getBody());
-					lnsJson = new ObjectMapper().readValue(response.getBody(), LnsJson.class);
-					
-					String strLnsJson = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(lnsJson);
-					log.info(">>>>> jsonNode             = {}", strLnsJson);
-				} catch (Exception e) {
-					//e.printStackTrace();
-					String message = e.getMessage();
-					log.error("ERROR >>>>> {}", message);
-					int pos1 = message.indexOf('[');
-					int pos2 = message.lastIndexOf(']');
-					lnsJson.setMessage(message.substring(pos1 + 1, pos2));
-				}
-			}
-			log.info("===============================================================");
-		}
-		
-		return lnsJson;
+		return new ResponseEntity<>(lnsJson, headers, HttpStatus.OK);
 	}
 }
