a44359a0a2df78f0d553ffad053579d632c8128b
grtlinux <grtlinux@gmail.com>
2020-11-13 00:24:45 +0900

    create

---
+++ ver-12.11/KieaLightnet-12-vfep-11/src/main/java/org/tain/KieaLightnet12Vfep11Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaLightnet12Vfep11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaLightnet12Vfep11Application.class, args);
+	}
+
+}
