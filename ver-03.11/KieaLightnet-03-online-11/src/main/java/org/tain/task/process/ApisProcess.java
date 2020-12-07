7a0aaca72743ba925550e557bf297d1706126540
grtlinux <grtlinux@gmail.com>
2020-12-07 21:08:25 +0900

    ...

--- ver-03.11/KieaLightnet-03-online-11/src/main/java/org/tain/task/process/ApisProcess.java
+++ ver-03.11/KieaLightnet-03-online-11/src/main/java/org/tain/task/process/ApisProcess.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Component;
 import org.tain.httpClient.LnsHttpClient;
 import org.tain.mapper.LnsJsonNode;
 import org.tain.object.lns.LnsStream;
+import org.tain.properties.ProjEnvUrlProperties;
 import org.tain.utils.CurrentInfo;
 import org.tain.utils.Flag;
 import org.tain.utils.JsonPrint;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,12 +19,16 @@ import lombok.extern.slf4j.Slf4j;
 public class ApisProcess {
 
 	@Autowired
+	private ProjEnvUrlProperties projEnvUrlProperties;
+	
+	@Autowired
 	private LnsHttpClient lnsHttpClient;
 	
 	public LnsStream process(LnsStream reqLnsStream) throws Exception {
 		log.info("KANG-20200908 >>>>> {}", CurrentInfo.get());
 		
-		String strReqJson = null;
+		/*
+		//String strReqJson = null;
 		if (Flag.flag) {
 			// 1. Stream to Json
 			LnsJsonNode lnsJsonNode = new LnsJsonNode();
@@ -34,9 +41,24 @@ public class ApisProcess {
 			
 			strReqJson = lnsJsonNode.getText("json");
 		}
+		*/
+		JsonNode reqJsonNode = null;
+		if (Flag.flag) {
+			// 1. Stream to Json
+			LnsJsonNode lnsJsonNode = new LnsJsonNode("{\"request\":{},\"response\":{}}");
+			lnsJsonNode.put("httpUrl", this.projEnvUrlProperties.getMapper() + "/mapper/s2j");
+			lnsJsonNode.put("httpMethod", "POST");
+			lnsJsonNode.put("/request", "reqResType", reqLnsStream.getTypeCode());
+			lnsJsonNode.put("/request", "stream", reqLnsStream.getData());
+			lnsJsonNode = this.lnsHttpClient.post(lnsJsonNode);
+			
+			reqJsonNode = lnsJsonNode.getJsonNode("/response", "json");
+			log.info("ONLINE-1 >>>>> after MAPPER.s2j {} = \n[{}]", lnsJsonNode.getText("/request", "reqResType"), reqJsonNode.toPrettyString());
+		}
 		
-		String strResJson = null;
-		String reqResType = null;
+		/*
+		//String strResJson = null;
+		//String reqResType = null;
 		if (Flag.flag) {
 			// 2. link
 			LnsJsonNode lnsJsonNode = new LnsJsonNode();
@@ -50,8 +72,26 @@ public class ApisProcess {
 			strResJson = lnsJsonNode.getText("resJson");
 			reqResType = lnsJsonNode.getText("reqResType");
 		}
+		*/
+		JsonNode resJsonNode = null;
+		String reqResType = null;
+		if (Flag.flag) {
+			// 2. link
+			LnsJsonNode lnsJsonNode = new LnsJsonNode("{\"request\":{},\"response\":{}}");
+			lnsJsonNode.put("httpUrl", this.projEnvUrlProperties.getLink() + "/link/process");
+			lnsJsonNode.put("httpMethod", "POST");
+			lnsJsonNode.put("/request", "reqResType", reqLnsStream.getTypeCode());
+			lnsJsonNode.put("/request", "json", reqJsonNode);
+			lnsJsonNode = this.lnsHttpClient.post(lnsJsonNode);
+			
+			resJsonNode = lnsJsonNode.getJsonNode("/response", "response");
+			reqResType = resJsonNode.at("/__head_data").asText();
+			log.info("ONLINE-2 >>>>> after LINK.process {} = \n{}", reqResType, resJsonNode.toPrettyString());
+			//strResJson = lnsJsonNode.getJsonNode("/response", "response").toPrettyString();
+			//log.info("ONLINE-2.2 >>>>> LINK_PROCESS lnsJsonNode.link {}    __body_data = \n{}", lnsJsonNode.getText("/request", "reqResType"), lnsJsonNode.getJsonNode("/response/response", "__body_data").toPrettyString());
+		}
 		
-		String strResStream = null;
+		/*
 		if (Flag.flag) {
 			// 3. Json to Stream
 			LnsJsonNode lnsJsonNode = new LnsJsonNode();
@@ -66,12 +106,27 @@ public class ApisProcess {
 			strResStream = lnsJsonNode.getText("stream");
 			log.info("ONLINE-4 >>>>> lnsJsonNode.ResStream = [{}]", strResStream);
 		}
+		*/
+		
+		String strResStream = null;
+		if (Flag.flag) {
+			// 2. Json to Stream
+			LnsJsonNode lnsJsonNode = new LnsJsonNode("{\"request\":{},\"response\":{}}");
+			lnsJsonNode.put("httpUrl", this.projEnvUrlProperties.getMapper() + "/mapper/j2s");
+			lnsJsonNode.put("httpMethod", "POST");
+			lnsJsonNode.put("/request", "reqResType", reqResType);
+			lnsJsonNode.put("/request", "json", resJsonNode);
+			lnsJsonNode = this.lnsHttpClient.post(lnsJsonNode);
+			
+			strResStream = lnsJsonNode.getText("/response", "stream");
+			log.info("ONLINE-3 >>>>> after MAPPER.j2s {} = \n[{}]", reqResType, strResStream);
+		}
 		
 		LnsStream resLnsStream = null;
 		if (Flag.flag) {
 			// 5. lnsStream
 			resLnsStream = new LnsStream(strResStream);
-			log.info("ONLINE-5 >>>>> resLnsStream = {}", JsonPrint.getInstance().toPrettyJson(resLnsStream));
+			log.info("ONLINE-4 >>>>> resLnsStream = {}", JsonPrint.getInstance().toPrettyJson(resLnsStream));
 		}
 		
 		return resLnsStream;
